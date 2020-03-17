ced9f2b61ca16f29bcde272a689dae5bd3a19d5e
mdekauwe <mdekauwe@gmail.com>
2020-03-17 23:04:03 +1100

    fix space issue

--- Model.R
+++ Model.R
@@ -43,15 +43,13 @@ model{
     for (m in 1:12) {
       # weight for precipitation received after the NPP harvest for
       # the current year is 0 (Oct,Nov,Dec)
-      delta[m, t] <-
-        (deltaX[block[t, m]]) * (1 - equals(t, 1) * step(m - 9.5))
+      delta[m, t] <- (deltaX[block[t, m]]) * (1 - equals(t, 1) * step(m - 9.5))
       # normalise the monthly weights (sumD is defined below)
       weight[m, t] <- delta[m, t] / sumD
       # Reorder the weights in order of "recentness" so that
       # they run from Dec of current year, Nov,... through to Feb,
       # Jan of oldest year as a vector rather than a matrix
-      weightOrdered[(t - 1) * 12 + (12 - m + 1)] <-
-        weight[m, t]
+      weightOrdered[(t - 1) * 12 + (12 - m + 1)] <- weight[m, t]
       # For each year of observations which has 5 previous years of
       # observations, compute the weighted precipitation variable.
       for (i in Nlag:Nyrs) {
@@ -63,6 +61,7 @@ model{
     # Sum the monthly weights for each lag year
     sumD1[t] <- sum(delta[, t])
   }
+  
   # Sum the yearly weights to get the total of all weights from over the
   # lag period considered
   sumD <- sum(sumD1[])
@@ -71,7 +70,6 @@ model{
   for (t in 1:(12 * Nlag)) {
     cum.weight[t] <- sum(weightOrdered[1:t])
   }
-  
   
   # Compute antecedent precipitation by summing the weighted precipitation
   # variable over months and past years:
@@ -85,8 +83,6 @@ model{
     # the previous 5 years
     antX[i] <- sum(ant.sum1[i, ])
   }
-  
-  
   
   # Specify the sd of the NPP likelihood
   
@@ -105,9 +101,8 @@ model{
   for (i in 1:N) {
     # Calculate mu, the mean of the distribution of NPP
     # (convert antecedent precipitation (antX) from inches to mm.)
-    mu[i] <-
-      a[1] + a[2] * antX[YearID[i]] * 25.4 + a[3] * Event[i, 1] +
-      a[4] * Event[i, 2] + a[5] * Event[i, 3] + a[6] * Event[i, 4]
+    mu[i] <- a[1] + a[2] * antX[YearID[i]] * 25.4 + a[3] * Event[i, 1] +
+             a[4] * Event[i, 2] + a[5] * Event[i, 3] + a[6] * Event[i, 4]
     # Likelihood for observed NPP - it is a normal distribution with
     # mean mu and sd tau
     NPP[i] ~ dnorm(mu[i], tau)
